cd2aec3468f971ea1f5b81e8d2f5afa6d9334a10
Kerry Smith <kerrysfs@gmail.com>
2020-09-15 20:58:49 -0700

    setup support for socketio,created new socketio instance server/client in js, setup client side socketio

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
+
+
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
 
 const port = process.env.PORT || 3000
-
 const publicDirectoryPath = path.join(__dirname, '../public') 
 // app.use(express.json())
 
 app.use(express.static(publicDirectoryPath))
 
 
+io.on('connection', () => {
+  console.log('New Websocket Connection...')
+})
 
 
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Server is running on ${port}!`)
 })
 
